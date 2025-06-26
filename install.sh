0958a24bd92ed0012b5eddfe6d1459394d2466e7
byddybbyd <178136455+byddybbyd@users.noreply.github.com>
2025-06-26 12:16:30 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ~/.termux
+rm termux.properties
+curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/termux.properties
+curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/font.ttf
