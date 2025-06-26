d025d80b9631dc0575874ab62660e7447a83ee8a
byddybbyd <178136455+byddybbyd@users.noreply.github.com>
2025-06-26 12:28:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ cd ~/.termux
 rm termux.properties
 curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/termux.properties
 curl -LO https://raw.githubusercontent.com/byddybbyd/new/refs/heads/main/font.ttf
+termux-reload-settings
+echo "Done"
